8aab66d01fae3c8678b121fe8ecec694427cd51e
lecterkn <lecterkn@proton.me>
2025-06-27 16:48:09 +0900

    オプションを変更 #1

--- lua/options.lua
+++ lua/options.lua
@@ -7,3 +7,11 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.fillchars = { eob = ' ' }
