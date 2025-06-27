09d0f519fb35815ad55000618e3c6e0e65300fbb
lecterkn <lecterkn@proton.me>
2025-06-27 16:01:54 +0900

    initial commit

---
+++ lua/keymap.lua
@@ -0,0 +1,10 @@
+vim.g.leader = " "
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+vim.keymap.set("v", "<leader>yy", '"+yy')
---
+++ lua/options.lua
@@ -0,0 +1,9 @@
+vim.opt.guicursor = ""
+vim.opt.relativenumber = true
+
+vim.opt.wrap = false
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
