e522d0f7fd00067d535df0f992b8d9f82e5e52ef
lecterkn <lecterkn@proton.me>
2025-07-16 13:38:38 +0900

    rose-pine-moonを設定

--- init.lua
+++ init.lua
@@ -2,4 +2,5 @@ require("config.lazy")
 require("options")
 require("keymap")
 
-vim.cmd("colorscheme catppuccin-mocha")
+-- vim.cmd("colorscheme catppuccin-mocha")
+vim.cmd("colorscheme rose-pine-moon")
