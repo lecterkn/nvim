78ca7601cb84fe747745fe5061bb7faa2b73e11f
hysilens <lecterkn@proton.me>
2025-08-08 00:05:50 +0900

    mason automatic setup

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -7,4 +7,9 @@ return {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
     },
+    config = function()
+        require("mason-lspconfig").setup {
+            automatic_enable = true
+        }
+    end
 }
