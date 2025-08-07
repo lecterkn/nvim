712a55506ec426981c1d41c5eef91ccbc2b598f0
hysilens <lecterkn@proton.me>
2025-08-08 00:03:16 +0900

    add vue settings for conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,23 +1,24 @@
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				go = { "goimports", "gofmt" },
-				markdown = { "prettier" },
-				html = { "prettier" },
-				css = { "prettier" },
-				json = { "prettier" },
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-			},
-			format_on_save = {
-				timeout_ms = 5000,
-				lsp_format = "fallback",
-			},
-		})
-	end,
+    "stevearc/conform.nvim",
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                go = { "goimports", "gofmt" },
+                markdown = { "prettier" },
+                html = { "prettier" },
+                css = { "prettier" },
+                json = { "prettier" },
+                vue = { "prettier" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+            },
+            format_on_save = {
+                timeout_ms = 5000,
+                lsp_format = "fallback",
+            },
+        })
+    end,
 }
